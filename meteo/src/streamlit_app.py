b7ae99d8ec80205a76683e4743081ec30381f133
jouahibou Diame <81402648+jouahibou@users.noreply.github.com>
2024-08-31 18:19:37 +0200

    Update streamlit_app.py

--- meteo/src/streamlit_app.py
+++ meteo/src/streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 from cassandra.cluster import Cluster
 import time
 
@@ -24,14 +25,49 @@ def fetch_weather_data_from_cassandra():
         st.error(f"Error executing query: {e}")
         return []
 
-st.title('Weather Data')
+st.title('Visualisation des données météorologiques')
 
 try:
     weather_data = fetch_weather_data_from_cassandra()
+    
     if not weather_data:
         st.write("No data found.")
     else:
+        
+        cities = []
+        temperatures = []
+        humidities = []
+        wind_speeds = []
+        
         for data in weather_data:
-            st.write(f"City: {data.city_name}, City: {data.city_name},Temperature: {data.temperature}°C, Humidity: {data.humidity}%, Wind Speed: {data.wind_speed} m/s")
+            cities.append(data.city_name)
+            temperatures.append(data.temperature)
+            humidities.append(data.humidity)
+            wind_speeds.append(data.wind_speed)
+        
+        
+        lengths = {len(cities), len(temperatures), len(humidities), len(wind_speeds)}
+        if len(lengths) > 1:
+            st.error("Data arrays have different lengths.")
+            st.write(f"Lengths: Cities: {len(cities)}, Temperatures: {len(temperatures)}, Humidities: {len(humidities)}, Wind Speeds: {len(wind_speeds)}")
+        else:
+           
+            data_dict = {
+                "City": cities,
+                "Temperature (°C)": temperatures,
+                "Humidity (%)": humidities,
+                "Wind Speed (m/s)": wind_speeds
+            }
+            
+            df = pd.DataFrame(data_dict)
+
+            
+            st.subheader('Tableau des données ')
+            st.dataframe(df)
+
+            
+            st.subheader('Température au-dessus de la ville')
+            st.line_chart(df.set_index('City')['Temperature (°C)'])
+
 except Exception as e:
     st.error(f"An error occurred: {e}")
